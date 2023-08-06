2bde19ceea91a5873811aa55fdf2afd6ef483347
William Robersone <william.robersone@gmail.com>
2023-08-06 00:24:47 -0500

    player

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
   songs: Song[];
@@ -16,6 +17,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
+
+    const onPlay = useOnPlay(songs);
 
     const onClick = () => {
       if (!user) {
@@ -40,7 +43,7 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
         {songs.map((song) => (
           <MediaItem 
             key={song.id}
-            onClick={() => {}}
+            onClick={(id: string) => onPlay(id)}
             data={song}
           />
         ))}
